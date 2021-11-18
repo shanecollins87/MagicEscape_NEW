1a50294c1ac660321ebcb49ac9cbdd1e97c7164a
colinmcelarney <71757780+colinmcelarney@users.noreply.github.com>
2021-11-18 23:19:34 +0000

    Added Files

---
+++ ButtonInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ButtonInteraction : MonoBehaviour
+{
+
+    public TextMeshProUGUI simpleUIText; 
+
+    public void OnButton1Clicked()
+    {
+        simpleUIText.text = "Loading Main Menu...";
+    }
+
+    public void OnButton2Clicked()
+    {
+        simpleUIText.text = "Loading Sonic Bells...";
+    }
+
+
+    public void OnButton3Clicked()
+    {
+        simpleUIText.text = "Loading CampFire...";
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# MagicEscape_
-New repo_
+# MagicEscape
+A relaxing VR experience. 
+
+03/11/2021
+To date we have basic project setup ready to be populated with files soon. Each member of the team has implemented their work on the main branch to get a solid template. In project are included visuals, sounds, UI and basic movement and interaction.
